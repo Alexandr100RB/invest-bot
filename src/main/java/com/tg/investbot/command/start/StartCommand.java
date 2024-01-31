6a6ee16acc84cd1f77ea38417334d8feab1e984a
aachuyko <aachuyko@yoomoney.ru>
2024-01-31 19:05:29 +0300

    Переехали на tinkoff-api

--- src/main/java/com/tg/investbot/command/start/StartCommand.java
+++ src/main/java/com/tg/investbot/command/start/StartCommand.java
@@ -5,12 +5,19 @@ import com.tg.investbot.command.UserCommand;
 import com.tg.investbot.registry.UserCommandName;
 import com.tg.investbot.model.StockInfo;
 import com.tg.investbot.repository.StocksInfoRepository;
+import org.hibernate.sql.results.graph.BiDirectionalFetch;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import ru.tinkoff.piapi.core.InvestApi;
+import ru.tinkoff.piapi.core.MarketDataService;
 
+import java.time.Instant;
 import java.util.List;
+import java.util.function.Function;
+import java.util.function.Supplier;
 
 import static com.tg.investbot.registry.Registry.COMMAND_REGISTRY;
+import static ru.tinkoff.piapi.contract.v1.CandleInterval.CANDLE_INTERVAL_1_MIN;
 
 /**
  * Команда старт {@link UserCommandName#START}
@@ -21,12 +28,15 @@ import static com.tg.investbot.registry.Registry.COMMAND_REGISTRY;
 public class StartCommand implements UserCommand {
     private final InvestBot investBot;
     private final StocksInfoRepository stocksInfoRepository;
+    private final InvestApi investApi;
 
     @Autowired
     public StartCommand(InvestBot investBot,
-                        StocksInfoRepository stocksInfoRepository) {
+                        StocksInfoRepository stocksInfoRepository,
+                        InvestApi investApi) {
         this.investBot = investBot;
         this.stocksInfoRepository = stocksInfoRepository;
+        this.investApi = investApi;
         COMMAND_REGISTRY.put(UserCommandName.START, this);
     }
 
@@ -42,5 +52,12 @@ public class StartCommand implements UserCommand {
             investBot.sendMessage(chatId, "Куплено " + stock.getQuantity()
                     + " шт " + stock.getTicker()+ " по " + stock.getBuyPrice());
         }
+        var candles = investApi.getMarketDataService()
+                .getCandles(
+                        "BBG004730N88",
+                        Instant.now(),
+                        Instant.now().plusMillis(100000),
+                        CANDLE_INTERVAL_1_MIN).join();
+        investBot.sendMessage(159776036, candles.toString());
     }
 }
